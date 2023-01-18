fc4723f8c8d6b9d14a1c3f6bbfe16bf5817b8bfc
tonimjdev <tonimdev@gmail.com>
2023-01-18 10:49:55 +0100

    203-205 Autocomplete cuando no encontró nada

--- src/app/heroes/pages/buscar/buscar.component.ts
+++ src/app/heroes/pages/buscar/buscar.component.ts
@@ -12,7 +12,7 @@ export class BuscarComponent implements OnInit {
   termino:string = "";
   heroes: Heroe[] = [];
 
-  heroeSeleccionado!: Heroe;
+  heroeSeleccionado!: Heroe | undefined;
 
   constructor( private heroesService: HeroesService ) { }
 
@@ -20,11 +20,17 @@ export class BuscarComponent implements OnInit {
   }
 
   buscando() {
-    this.heroesService.getSugerencias( this.termino )
+    this.heroesService.getSugerencias( this.termino.trim() )
     .subscribe( heroes => this.heroes = heroes );
   }
 
   opcionSeleccionada( event: MatAutocompleteSelectedEvent ) {
+    if(!event.option.value) {
+      this.heroeSeleccionado = undefined;
+      return;
+    }
+    
+    
     const heroe: Heroe = event.option.value;
     this.termino = heroe.superhero;
 
